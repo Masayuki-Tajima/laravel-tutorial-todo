873bc7050f357c3597d4dcb3b114d1b36165f127
Masayuki-Tajima <masatajima12@gmail.com>
2024-11-21 21:05:34 +0900

    trash.blade.phpビューファイルを作成

---
+++ resources/views/tasks/trash.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todoリストーゴミ箱</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+
+</body>
+</html>
