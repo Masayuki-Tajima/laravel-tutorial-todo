7aa2d6997808055ad97fca7482c4d9c05f42ed51
Masayuki-Tajima <masatajima12@gmail.com>
2024-11-21 20:46:06 +0900

    index.blade.phpにcssを反映

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todoリスト</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100">
     <div class="container mx-auto p-4">
